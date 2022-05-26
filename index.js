d9bb66cbdce16799daf05fca450b46dec2934afd
Всеволод <63463168+vsecoder@users.noreply.github.com>
2022-05-26 11:41:46 +0300

    Fix bug
    tag <a> glitch

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 // get all <p>
 var paragraphs = document.getElementsByTagName("p");
+var regex1 = /(<([^>]+))/ig
+var regex2 = /(([^>]+)>)/ig
+var t = 0;
 
 // cycle through
 for(var i = 0; i < paragraphs.length; i++) {
     // get <p> text
-    let text = paragraphs[i].innerText
+    let text = paragraphs[i].innerHTML
     // splitting the text
     let words = text.split(" ")
     let result = ''
     // process every word
     words.map((elem) => {
-        let mid = Math.floor(elem.length / 2)
-        result += `<span style="font-weight: bold">${elem.slice(0, mid)}</span>${elem.slice(mid)} `
+        if (elem == '<a') {t = 1};
+        if (t == 1) {
+            if (elem.slice(-1) == '>') {t = 0}
+            return result += `${elem} `
+        }
+        if (elem.match(regex1) == null || elem.match(regex2) == null) {
+            let mid = Math.floor(elem.length / 2);
+            return result += `<span style="font-weight: bold">${elem.slice(0, mid)}</span>${elem.slice(mid)} `;
+        } else {
+            return result += `${elem} `;
+        }
+
     });
     //console.log(result);
     // write result
